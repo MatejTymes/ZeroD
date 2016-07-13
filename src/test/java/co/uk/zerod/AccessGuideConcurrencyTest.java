ffc6177760a25aeffed67752f21b6b5abf1494e9
mtymes <matejtymes@gmail.com>
2016-07-13 20:41:33 +0100

    mtymes: adding skeleton of concurrency test

---
+++ src/test/java/co/uk/zerod/AccessGuideConcurrencyTest.java
@@ -0,0 +1,31 @@
+package co.uk.zerod;
+
+import org.junit.Test;
+
+public class AccessGuideConcurrencyTest {
+
+    @Test
+    public void shouldBlockOnSwitchUntilAllOldReadStateMethodsFinish() {
+        // todo: implement
+    }
+
+    @Test
+    public void shouldBlockOnSwitchUntilAllOldWriteStateMethodsFinish() {
+        // todo: implement
+    }
+
+    @Test
+    public void shouldNotSwitchStateUntilPreviousSwitchFinished() {
+        // todo: implement
+    }
+
+    @Test
+    public void shouldUseNewReadStatusWhileSwitching() {
+        // todo: implement
+    }
+
+    @Test
+    public void shouldUseNewWriteStatusWhileSwitching() {
+        // todo: implement
+    }
+}
